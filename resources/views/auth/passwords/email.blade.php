ecbe37cf4da14b0b4a1b1b0f8351a2ae4a399e71
ediBoardMaster <edi.board.master@gmail.com>
2019-08-11 00:25:53 +0800

    Create email blade, using Auth layout

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,36 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<h3>FORGOT PASSWORD?</h3>
+<p>Try that again, and if it still doesn't work, let us know.</p>
+@endsection
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+@section('card-content')
+<div class="header">
+    <h5>Forgot Password?</h5>
+    <span>Enter your e-mail address below to reset your password.</span>
 </div>
+<form class="form" method="POST" action="{{ route('password.email') }}">
+
+    @csrf
+
+    <div class="input-group">
+        <input placeholder="Enter Email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        <span class="input-group-addon"><i class="zmdi zmdi-email"></i></span>
+        
+
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+
+    </div>
+
+    <div class="footer">
+        <button class="btn btn-primary btn-round btn-block" type="submit">SUBMIT</button>
+    </div>
+
+</form>
+<a href="javascript:void(0);" class="link">Need Help?</a>
 @endsection
